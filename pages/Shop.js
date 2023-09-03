a185dade889ce2c76cf03979d1e2fb00888cce8a
Jovana Savic <joocaasavic@gmail.com>
2023-09-03 09:57:57 +0200

    Napravljen Dark mode na Shop stranici

--- pages/Shop.js
+++ pages/Shop.js
@@ -2,10 +2,24 @@ import React from "react";
 import { ShopList } from "../data/ShopList";
 import ShopItem from "../components/ShopItem";
 import "../styles/Shop.css";
+import { createContext, useState } from "react";
+import ReactSwitch from "react-switch";
+
+export const ThemeContext = createContext(null);
 
 function Shop() {
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
   return (
-    <div className="shop">
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+    <div className="shop" id={theme}>
+      <div className="switch">
+        <label> {theme === "light" ? "Light Mode" : "Dark Mode"}</label>
+        <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
+      </div>
       <h1 className="shopTitle">Our Shop</h1>
       <div className="shopList">
         {ShopList.map((shopItem, key) => {
@@ -19,8 +33,12 @@ function Shop() {
           );
         })}
       </div>
-    </div>
+      </div>
+
+      </ThemeContext.Provider>
   );
-}
+      }
+    
+
 
 export default Shop;
